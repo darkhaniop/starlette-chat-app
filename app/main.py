302ba5dc3a280015c74e87973192038655fc23af
darkhaniop <72267237+darkhaniop@users.noreply.github.com>
2025-04-14 07:48:59 +0900

    add the chat page route
    The HTML template comes from the FastAPI docs. Overall, the page
    markup does not matter a lot here. It just allows connecting to the
    `/ws` endpoint to send and receive text messages via a WebSocket
    connection.

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from hypercorn.config import Config
 from hypercorn.typing import Framework
 from starlette.applications import Starlette
 from starlette.requests import Request
-from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse, HTMLResponse
 from starlette.routing import Route, WebSocketRoute
 from starlette.websockets import WebSocket
 
@@ -19,6 +19,45 @@ async def websocket_endpoint(websocket: WebSocket) -> None:
         await websocket.send_text(f"Message text was: {data}")
 
 
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>WebSocket Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://localhost:8080/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+async def chat_response(_request: Request) -> HTMLResponse:
+    return HTMLResponse(html)
+
+
 def create_app() -> Starlette:
     async def homepage(_request: Request) -> JSONResponse:
         return JSONResponse({"result": "Hello World!"})
@@ -27,6 +66,7 @@ def create_app() -> Starlette:
         debug=True,
         routes=[
             Route("/", homepage),
+            Route("/chat", chat_response),
             WebSocketRoute("/ws", websocket_endpoint),
         ],
     )
